8278cb5be3832a6dd3a3fe0dcd5a1eb62396f22b
Boatrite <boatrite@mobiledoorman.com>
2019-10-17 22:55:54 -0500

    Start moving code over to a Window class
    Want to keep main pretty slim, idk maybe this is dumb though. Might
    revert.
    
    Maybe it's okay for main to just do all this window stuff. Seems fine to
    me.
    
    Maybe extra structure will be nice once we have a main and renderer and
    game logic and such.

--- src/main.cpp
+++ src/main.cpp
@@ -32,48 +32,60 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
 }
 #pragma GCC diagnostic pop
 
+class Window {
+  public:
+    Window() {
+      std::cout << "Window created" << std::endl;
+    }
+
+    int show() {
+      glfwSetErrorCallback(error_callback);
+
+      glfwInit();
+      glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+      glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+      glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+      GLFWwindow* window = glfwCreateWindow(800, 600, "Mutiny :)", NULL, NULL);
+      if (window == NULL) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+      }
+      glfwMakeContextCurrent(window);
+
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+      glfwSetKeyCallback(window, key_callback);
+      glfwSetScrollCallback(window, scroll_callback);
+      glfwSetCursorPosCallback(window, mouse_callback);
+
+      if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+      }
+
+      glViewport(0, 0, 800, 600);
+      glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+      glfwSetTime(0.0);
+      double last_time = 0.0;
+      while(!glfwWindowShouldClose(window)) {
+        double time = glfwGetTime();
+        double dt = time - last_time;
+        last_time = time;
+        std::cout << "dt: " << dt << std::endl;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+      }
+
+      glfwTerminate();
+      return 0;
+    }
+};
+
 int main() {
-  glfwSetErrorCallback(error_callback);
-
-  glfwInit();
-  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  GLFWwindow* window = glfwCreateWindow(800, 600, "Mutiny :)", NULL, NULL);
-  if (window == NULL) {
-    std::cerr << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-
-  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-  glfwSetKeyCallback(window, key_callback);
-  glfwSetScrollCallback(window, scroll_callback);
-  glfwSetCursorPosCallback(window, mouse_callback);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cerr << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-
-  glViewport(0, 0, 800, 600);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-  glfwSetTime(0.0);
-  double last_time = 0.0;
-  while(!glfwWindowShouldClose(window)) {
-    double time = glfwGetTime();
-    double dt = time - last_time;
-    last_time = time;
-    std::cout << "dt: " << dt << std::endl;
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-
-  glfwTerminate();
-  return 0;
+  Window* window = new Window();
+  return window->show();
 }
