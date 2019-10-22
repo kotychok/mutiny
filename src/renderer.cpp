ccf0db8f4658ca1897672c2898c6c4547f01095d
Boatrite <boatrite@mobiledoorman.com>
2019-10-21 20:58:51 -0500

    Render a triangle
    I didn't need to use class members as much as I did, but I'll want stuff
    like this eventually I think... so good to figure out I guess.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -6,9 +6,35 @@
 
 Renderer::Renderer() {
   std::cout << "Renderer created" << std::endl;
+  glGenBuffers(1, &vbo);
+  glGenVertexArrays(1, &vao);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  unsigned int shaderProgram { glCreateProgram() };
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 }
 
 void Renderer::render(double dt) {
-  glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
+  glClearColor(0.2f * dt, 0.3f, 0.3f, 0.1f); // Use dt here for now to get rid of the warning while I fill out the rest of the missing code. It's a nice color.
   glClear(GL_COLOR_BUFFER_BIT);
+
+  glBindVertexArray(vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -1,6 +1,31 @@
 #pragma once
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 class Renderer {
+  private:
+    float vertices[9] {
+      -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    };
+    unsigned int vbo {};
+    unsigned int vao {};
+    unsigned int vertexShader { glCreateShader(GL_VERTEX_SHADER) };
+    unsigned int fragmentShader { glCreateShader(GL_FRAGMENT_SHADER) };
+    const char *vertexShaderSource { "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0" };
+    const char *fragmentShaderSource { "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main()\n"
+      "{\n"
+      "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+      "}\n\0" };
   public:
     Renderer();
     void render(double dt);
