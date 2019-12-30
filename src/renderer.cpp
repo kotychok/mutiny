8bf9e050a1236f7328f06fffdd16377ec0da16b1
Gwen Boatrite <boatrite@mobiledoorman.com>
2019-12-29 22:30:34 -0600

    Render a texture

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "stb/stb_image.h"
+
 #include "renderer.h"
 #include "shader.h"
 
@@ -9,9 +11,37 @@ Renderer::Renderer() {
   std::cout << "Renderer created" << std::endl;
   glGenBuffers(1, &vbo);
   glGenVertexArrays(1, &vao);
+  glGenBuffers(1, &ebo);
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load("./assets/container.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // glTexStorage2D segfaults for some reason.
+    //
+    // glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGB8, width, height);
+    // glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+  } else {
+    std::cerr << "ERROR: Failed to load image" << std::endl;
+  }
+  stbi_image_free(data);
 
   Shader myShader("./src/shaders/vertex.vert", "./src/shaders/fragment.frag");
   myShader.use();
+  myShader.setInt("ourTexture", 0);
 }
 
 void Renderer::render(double dt) {
@@ -23,8 +53,18 @@ void Renderer::render(double dt) {
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -5,13 +5,20 @@
 
 class Renderer {
   private:
-    float vertices[9] {
-      -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+    float vertices[32] {
+      // positions          // colors           // texture coords
+       0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+       0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+      -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+      -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    };
+    unsigned int indices[6] {
+      0, 1, 3,
+      1, 2, 3,
     };
     unsigned int vbo {};
     unsigned int vao {};
+    unsigned int ebo {};
   public:
     Renderer();
     void render(double dt);
