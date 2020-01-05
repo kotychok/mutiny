717642f0a45d01ac11f717b5f0a24e4a9205fc39
Gwen Boatrite <boatrite@mobiledoorman.com>
2020-01-05 11:36:57 -0600

    Align y = 0 with the bottom of the chunk
    Instead of the bottom + 1.

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -12,7 +12,7 @@ void Chunk::render(const Shader &myShader, const glm::mat4 &chunkModel) {
     for (unsigned int z = 0; z < Chunk::SIZE; z++) {
       for (unsigned int y = 0; y < Chunk::SIZE; y++) {
         glm::mat4 blockModel = glm::translate(chunkModel, glm::vec3(x, y, z));
-        blockModel = glm::translate(blockModel, glm::vec3(0.5f, -0.5f, 0.5f));
+        blockModel = glm::translate(blockModel, glm::vec3(0.5f, 0.5f, 0.5f));
         blockModel = glm::translate(blockModel, pos * Chunk::SIZE);
         blockModel = glm::translate(blockModel, -glm::vec3(Chunk::SIZE / 2, 0, Chunk::SIZE / 2));
         myShader.setMat4("model", blockModel);
