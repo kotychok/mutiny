0e1dbd20348217c5419728f76e214d41586ae31f
Gwen Boatrite <boatrite@mobiledoorman.com>
2020-01-12 22:18:28 -0600

    Replace demo window with simple overlay example
    Will be modifying this with info I want to always have displayed.
    
    Will likely be pulling more info out of demo for this. Anything
    profiling related.

--- src/window.cpp
+++ src/window.cpp
@@ -9,6 +9,8 @@
 #include "renderer.h"
 #include "window.h"
 
+void ShowExampleAppSimpleOverlay(bool* p_open);
+
 Window::Window() {
   std::cout << "Window created" << std::endl;
 }
@@ -45,10 +47,9 @@ int Window::show() {
   ImGui_ImplGlfw_InitForOpenGL(window, true);
   const char* glslVersion = "#version 130";
   ImGui_ImplOpenGL3_Init(glslVersion);
-  bool show_demo_window = true;
-
   ImGuiIO& io = ImGui::GetIO();
   io.IniFilename = NULL;
+  bool showOverlay = true;
 
   glViewport(0, 0, WIDTH, HEIGHT);
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
@@ -74,7 +75,9 @@ int Window::show() {
     ImGui::NewFrame();
 
     // Show a demo window
-    ImGui::ShowDemoWindow(&show_demo_window);
+    if (showOverlay) {
+      ShowExampleAppSimpleOverlay(&showOverlay);
+    }
 
     if (isFocusedInGame(window)) {
       renderer.processInput(window, dt);
@@ -175,4 +178,38 @@ void Window::focusInGUI(GLFWwindow* window) {
   ImGuiIO& io = ImGui::GetIO();
   io.ConfigFlags &= ~ImGuiConfigFlags_NoMouse;          // Enable Mouse
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard
+}
+
+void ShowExampleAppSimpleOverlay(bool* p_open)
+{
+    const float DISTANCE = 10.0f;
+    static int corner = 0;
+    ImGuiIO& io = ImGui::GetIO();
+    if (corner != -1)
+    {
+        ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
+        ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
+        ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+    }
+    ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+    if (ImGui::Begin("Example: Simple overlay", p_open, (corner != -1 ? ImGuiWindowFlags_NoMove : 0) | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
+    {
+        ImGui::Text("Simple overlay\n" "in the corner of the screen.\n" "(right-click to change position)");
+        ImGui::Separator();
+        if (ImGui::IsMousePosValid())
+            ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
+        else
+            ImGui::Text("Mouse Position: <invalid>");
+        if (ImGui::BeginPopupContextWindow())
+        {
+            if (ImGui::MenuItem("Custom",       NULL, corner == -1)) corner = -1;
+            if (ImGui::MenuItem("Top-left",     NULL, corner == 0)) corner = 0;
+            if (ImGui::MenuItem("Top-right",    NULL, corner == 1)) corner = 1;
+            if (ImGui::MenuItem("Bottom-left",  NULL, corner == 2)) corner = 2;
+            if (ImGui::MenuItem("Bottom-right", NULL, corner == 3)) corner = 3;
+            if (p_open && ImGui::MenuItem("Close")) *p_open = false;
+            ImGui::EndPopup();
+        }
+    }
+    ImGui::End();
 }
