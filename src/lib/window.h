09a5b1be2691a10aaa243261bd12ae2bb88c4da2
Gwen Boatrite <gwen.boatrite@gmail.com>
2020-01-22 22:05:32 -0600

    Add OpenGL debug callback function

--- src/lib/window.h
+++ src/lib/window.h
@@ -13,6 +13,7 @@ class Window {
     static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
     static void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
     static void cursorPosCallback(GLFWwindow* window, double xpos, double ypos);
+    static void GLAPIENTRY glDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam);
 
     static bool isFocusedInGame(GLFWwindow* window);
     static bool isFocusedInGUI(GLFWwindow* window);
